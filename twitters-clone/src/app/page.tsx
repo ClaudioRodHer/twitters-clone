9852bd68138328db2bd3532cb827c96b6fdfb3d4
ClaudioRodHer <isaiasclaudio777@gmail.com>
2023-10-04 14:56:49 -0600

    left bar 1

--- twitters-clone/src/app/page.tsx
+++ twitters-clone/src/app/page.tsx
@@ -1,10 +1,13 @@
 import {BiHomeCircle} from 'react-icons/bi'
 import {HiOutlineHashtag} from 'react-icons/hi'
-import {BsBell} from 'react-icons/bs'
+import {BsBell, BsTwitter} from 'react-icons/bs'
 import {HiEnvelope} from 'react-icons/hi2'
 import {BsBookmark} from 'react-icons/bs'
 import {BiUser} from 'react-icons/bi'
 import {CiCircleMore} from 'react-icons/ci'
+import Link from 'next/link'
+
+
 const NAVIGATION_ITEMS=[
   {
     title:'Home',
@@ -38,27 +41,38 @@ const NAVIGATION_ITEMS=[
 ]
 export const Home = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center relative bg-black'>
-      <div className='max-w-scree-lg w-full h-full flex relative '>
-        {/**left side bar navigation/ header */}
-        <section className='fixed w-72 flex flex-col'>left section
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        {/**left side bar navigation/ header*/}
+
+        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
+          <Link href={'/'} className="p-2 text-2xl">
+           <BsTwitter/>
+          </Link>
           {
             NAVIGATION_ITEMS.map((item)=>(
-              <link className='bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4' href={`/${item.title.toLowerCase}`} key={item.title}>
+              /*los links se deben definir*/
+              <Link 
+              className="hover:bg-white/10 text-2xl transition duration-200 
+              flex items-center justify-start w-fit space-x-2 rounded-3xl 
+              py-2 px-6" 
+              href={`/${item.title.toLowerCase()}`} 
+              key={item.title}>
                 <div> 
-                  <item.icon />
+                  <item.icon/>
                 </div>
                 <div>
                   {
                     item.title
                   }
                 </div>
-              </link>
+              </Link>
             ))
           }
         </section>
-        <main>Home Time line </main>
-        <section>right section</section>
+        
+        {/*<main>Home Time line</main>
+        <section>right section</section>*/}
       </div>
     </div>
   )
